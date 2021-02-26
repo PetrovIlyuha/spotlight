fd60ea099fbe13955d093fdf1884a2804f4d07d2
Ilya <dnamix1@gmail.com>
2021-02-26 12:19:15 +0300

    add generated graphql schemas and _app modified

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { themeLight, themeDark } from 'lib/theme';
