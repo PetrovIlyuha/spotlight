b9379be47683e045e8d7e518093dff96ad8486fb
Ilya <dnamix1@gmail.com>
2021-02-26 12:18:01 +0300

    change config files for working with images from next and added framer-motion animatins

---
+++ client/motion/animations.ts
@@ -0,0 +1,14 @@
+export const slideInLeft = {
+  hidden: {
+    opacity: 0,
+    x: -200,
+  },
+  show: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 1,
+      delay: 0.3,
+    },
+  },
+};
